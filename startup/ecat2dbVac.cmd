5a432eadf4a609190df5a4160d4bcdee5dd14088
Alexander Söderqvist <alexander.soderqvist@esss.se>
2016-10-13 18:44:53 +0200

    Tested vacuum ioc

--- startup/ecat2dbVac.cmd
+++ startup/ecat2dbVac.cmd
@@ -2,6 +2,6 @@ ecat2configure(0,500,1,1)
 
 epicsEnvSet("PREFIX", "ECAT2TEST")
 
-dbLoadRecords("el3164.db", "PREFIX=$(PREFIX), MOD_ID=AIMOD0, SLAVE_IDX=1")
-dbLoadRecords("el3164.db", "PREFIX=$(PREFIX), MOD_ID=AIMOD1, SLAVE_IDX=2")
-dbLoadRecords("el3164.db", "PREFIX=$(PREFIX), MOD_ID=AIMOD2, SLAVE_IDX=3")
+dbLoadRecords("ecat2el3164.db", "PREFIX=$(PREFIX), MOD_ID=AIMOD0, SLAVE_IDX=1")
+dbLoadRecords("ecat2el3164.db", "PREFIX=$(PREFIX), MOD_ID=AIMOD1, SLAVE_IDX=2")
+dbLoadRecords("ecat2el3164.db", "PREFIX=$(PREFIX), MOD_ID=AIMOD2, SLAVE_IDX=3")
