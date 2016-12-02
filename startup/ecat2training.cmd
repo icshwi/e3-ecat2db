16544a0721fc67441d02d3ecce0c3b3c0f423ad4
Alexander Söderqvist <alexander.soderqvist@esss.se>
2016-12-02 14:12:34 +0000

    Added training startup script

--- startup/ecat2training.cmd
+++ startup/ecat2training.cmd
@@ -1,9 +1,12 @@
 require ecat2
-require ecat2db,alexandersoderqvist
+require ecat2db
 
-ecat2configure(0,100,1,1)
+epicsEnvSet("PREFIX", "$(PREFIX=ECAT2)")
+epicsEnvSet("FREQ", "$(FREQ=1000)")
 
-dbLoadRecords("ecat2el3602.db", "PREFIX=ECAT2, MOD_ID=AIMOD0, SLAVE_IDX=1")
-dbLoadRecords("ecat2el4134.db", "PREFIX=ECAT2, MOD_ID=AOMOD0, SLAVE_IDX=2")
-dbLoadRecords("ecat2el1014.db", "PREFIX=ECAT2, MOD_ID=DIMOD0, SLAVE_IDX=3")
-dbLoadRecords("ecat2el2004.db", "PREFIX=ECAT2, MOD_ID=DOMOD0, SLAVE_IDX=4")
+ecat2configure(0,$(FREQ),1,1)
+
+dbLoadRecords("ecat2el3602.db", "PREFIX=$(PREFIX), MOD_ID=AIMOD0, SLAVE_IDX=1")
+dbLoadRecords("ecat2el4134.db", "PREFIX=$(PREFIX), MOD_ID=AOMOD0, SLAVE_IDX=2")
+dbLoadRecords("ecat2el1014.db", "PREFIX=$(PREFIX), MOD_ID=DIMOD0, SLAVE_IDX=3")
+dbLoadRecords("ecat2el2004.db", "PREFIX=$(PREFIX), MOD_ID=DOMOD0, SLAVE_IDX=4")
